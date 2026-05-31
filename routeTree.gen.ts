e4507d69b967554168e1bb4d5bebf3ed00bc9c31
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-05-31 10:32:12 +0530

    Update routeTree.gen.ts

--- routeTree.gen.ts
+++ routeTree.gen.ts
@@ -1,11 +1,4 @@
 pp
   id: '/',
   path: '/',
-  getParentRoute: () => rootRouteImport,
-} as any)
-
-export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-}
-export interface FileRoutesByTo {
-00
+  getParentRouteoo
