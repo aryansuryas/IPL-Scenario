54095f0f7a601b5a0a0d4c9607f2142bc8828ec2
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-05-31 10:02:59 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,23 +1,7 @@
 import "./lib/error-capture";
 
 import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
-
-type ServerEntry = {
-  fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
-};
-
-let serverEntryPromise: Promise<ServerEntry> | undefined;
-
-async function getServerEntry(): Promise<ServerEntry> {
-  if (!serverEntryPromise) {
-    serverEntryPromise = import("@tanstack/react-start/server-entry").then(
-      (m) => ((m as { default?: ServerEntry }).default ?? (m as unknown as ServerEntry)),
-    );
-  }
-  return serverEntryPromise;
-}
-
+import { ro=
 function brandedErrorResponse(): Response {
   return new Response(renderErrorPage(), {
     status: 500,
