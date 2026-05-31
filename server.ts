1564652ecd4fe1b2e4894a0e69f2c99cc3d4384c
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-05-31 10:03:21 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,14 +1,1 @@
-ioo
-
-export default {
-  async fetch(request: Request, env: unknown, ctx: unknown) {
-    try {
-      const handler = await getServerEntry();
-      const response = await handler.fetch(request, env, ctx);
-      return await normalizeCatastrophicSsrResponse(response);
-    } catch (error) {
-      console.error(error);
-      return brandedErrorResponse();
-    }
-  },
-};
+oo
