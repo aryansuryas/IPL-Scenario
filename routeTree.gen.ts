6b77e9c0a14ede26be7e36729a02c714f5a728f2
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-05-31 10:32:03 +0530

    Update routeTree.gen.ts

--- routeTree.gen.ts
+++ routeTree.gen.ts
@@ -8,25 +8,4 @@ export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
 }
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-}
-export interface FileRoutesById {
-  __root__: typeof rootRouteImport
-  '/': typeof IndexRoute
-}
-export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
-  fileRoutesByTo: FileRoutesBy
-  fileRoutesById: FileRoutesById
-}
-    }
-  }
-}
-
-const rootRouteChildren: RootRouteChildren = {
-  IndexRoute: IndexRoute,
-}
-export const routeTree = rootRouteImport
-  ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+00
